9ba95061d102183d0a71eef802c50cb936de1079
MystixCode <51506512+MystixCode@users.noreply.github.com>
2020-05-31 12:32:07 +0200

    add collisionshapes generation for godot

--- TerrainGrid.py
+++ TerrainGrid.py
@@ -1,37 +1,32 @@
-import bpy, bmesh, mathutils
+import bpy, bmesh, mathutils, os
 from bpy import context as C
 ##Attention!: before using script move terrain so that its corner is at x=0,y=0 in positiv direction and apply transforms!
-print("########################\n## terrain grid start ##\n########################")
-print("-bisect and separate mesh")
+os.system('cls' if os.name == 'nt' else "printf '\033c'")
+print("## terrain grid start\n################################################")
+print(" - bisect and separate mesh")
 chunksize = 16
 bpy.ops.object.mode_set(mode='EDIT')
 bm = bmesh.from_edit_mesh(C.object.data)
 edges = []
-
 x, y, z = bpy.context.active_object.dimensions
 bsaxis = None
 if y>x:
     bsaxis = int(y)
 else:
-    bsaxis = int(x)
-        
+    bsaxis = int(x)  
 for i in range(0, bsaxis, chunksize):
         ret = bmesh.ops.bisect_plane(bm, geom=bm.verts[:]+bm.edges[:]+bm.faces[:], plane_co=(i,0,0), plane_no=(-1,0,0))
         bmesh.ops.split_edges(bm, edges=[e for e in ret['geom_cut'] if isinstance(e, bmesh.types.BMEdge)])
-
 for i in range(chunksize, bsaxis, chunksize):
         ret = bmesh.ops.bisect_plane(bm, geom=bm.verts[:]+bm.edges[:]+bm.faces[:], plane_co=(0,i,0), plane_no=(0,1,0))
         bmesh.ops.split_edges(bm, edges=[e for e in ret['geom_cut'] if isinstance(e, bmesh.types.BMEdge)])
-        
 bmesh.update_edit_mesh(C.object.data)
 bpy.ops.mesh.separate(type='LOOSE')
 bpy.ops.object.mode_set(mode='OBJECT')
 
-print("-set origin for new objects")
+print(" - set origin for new objects")
 for ob in bpy.data.objects: 
-    for obj in bpy.data.objects:
-        obj.select_set(False)
-    
+    bpy.ops.object.select_all(action='DESELECT')
     if ob.type == 'MESH':
         ob.select_set(True)
         mw = ob.matrix_world 
@@ -41,13 +36,10 @@ for ob in bpy.data.objects:
         bpy.context.scene.cursor.location = (minX, minY, 0.0)
         bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
         bpy.context.scene.cursor.location = (0.0, 0.0, 0.0)
-        #bpy.ops.object.transform_apply(location = True, scale = True, rotation = True)
 
-print("-connect objects on top of eachother")
+print(" - join objects if on top of eachother")
 for ob in bpy.context.view_layer.objects:
-    for obj in bpy.data.objects:
-        obj.select_set(False)
-        
+    bpy.ops.object.select_all(action='DESELECT') 
     if ob.type == 'MESH':
         for ob2 in bpy.context.view_layer.objects:
             if ob2.type == 'MESH' and ob != ob2:
@@ -57,10 +49,19 @@ for ob in bpy.context.view_layer.objects:
                         ob.select_set(True)
                         bpy.context.view_layer.objects.active = ob     
                         bpy.ops.object.join()
-                        
-print("-renaming object")
-for objx in bpy.context.view_layer.objects:
-    objx.name = "chunk" + str(objx.location.x) + "_" + str(objx.location.y)
-    objx.data.name = "chunk" + str(objx.location.x) + "_" + str(objx.location.y)
 
+print(" - rename and duplicate for collisionshapes for godot")
+for objy in bpy.data.objects:
+    bpy.ops.object.select_all(action='DESELECT')
+    if objy.type == 'MESH':
+        objy.name = "chunk" + str(objy.location.x) + "_" + str(objy.location.y)
+        objy.data.name = "chunk" + str(objy.location.x) + "_" + str(objy.location.y)
+        objy.select_set(True)
+        bpy.ops.object.duplicate()
+        for objz in bpy.context.selected_objects:
+            objz.name = objy.name + "-colonly"
+            objz.data.name = objy.name + "-colonly"
+            objz.parent = objy
+            objz.matrix_parent_inverse = objy.matrix_world.inverted()
 print("terrain grid done")
+#bpy.ops.object.transform_apply(location = True, scale = True, rotation = True)
